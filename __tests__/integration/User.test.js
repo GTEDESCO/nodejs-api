ac005c02cc09c4c4226cf69c8e3daffc959ed584
Gustavo Tedesco <gtedesco>
2019-10-16 11:02:10 -0300

    dados fakes testes

--- __tests__/integration/User.test.js
+++ __tests__/integration/User.test.js
@@ -2,15 +2,20 @@ import request from 'supertest';
 
 import app from '../../src/app';
 
+import factory from '../factories';
+import truncate from '../util/truncate';
+
 describe('User', () => {
+  beforeEach(async () => {
+    await truncate();
+  });
+
   it('deve ser possivel se cadastrar', async () => {
+    const user = await factory.attrs('User');
+
     const response = request(app)
       .post('/users')
-      .send({
-        name: 'teste',
-        email: 'teste@teste.com',
-        password: '123456',
-      });
+      .send(user);
 
     expect(response.body).toHaveProperty('id');
   });
